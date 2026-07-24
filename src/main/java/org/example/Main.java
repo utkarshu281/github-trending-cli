637aeb3f7d16205e1b17a5553271a34a042d9310
utkashu281 <utkarshu888@gmail.com>
2026-07-24 16:16:18 +0530

    build cli input

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import picocli.CommandLine;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@CommandLine.Command(name="GithubtrendingCLI", mixinStandardHelpOptions = true)
+public class Main implements Runnable {
+    @CommandLine.Option(names = {"--d", "--duration"}, defaultValue = "WEEK", description = "Duration")
+    Duration duration;
+    @CommandLine.Option(names = {"--l", "--limit"}, description = "limit the result")
+    private int value=10;
+
+    @Override
+    public void run() {
+        System.out.println(duration +"and"+ value);
+    }
+
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new Main()).execute(args));
+    }
+
+    enum Duration {
+        MONTH, YEAR, WEEK, DAY
+    }
+}
